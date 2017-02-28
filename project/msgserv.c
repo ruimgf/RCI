e913a189e48e726434b7f585e28a16bd0bb5bea1
Rui Figueiredo <ruimgf95@gmail.com>
2017-02-28 16:32:29 +0000

    add script to run

--- project/msgserv.c
+++ project/msgserv.c
@@ -8,41 +8,59 @@
 
 void wrong_use(){
   printf("Wrong Program Usage : msgserv –n name –j ip -u upt –t tpt [-i siip] [-p sipt] [–m m] [–r r] \n");
-  exit(-1);
+  //exit(-1);
 }
 
 int main(int argc, char *argv[]){
   char buffer[BUFFERSIZE];
   char command[BUFFERSIZE];
-  char name[] = "rui";
-  char ip[] = "127.0.0.1";
-  char upt[] = "9000";
-  char tpt[] = "10000";
+  char name[100];
+  char ip[100];
+  char upt[100];
+  char tpt[100];
   char ip_tejo[]  = "193.136.138.142";
   int dns_port = 59000;
-  char test_reg[] = "REG test1234;127.0.0.1;1000;2000\n";
+  char test_reg[100];
 
-  int fd_id_server = udp_connect(ip_tejo,dns_port);
-  printf("go WRITE\n");
-  udp_write_to(fd_id_server,test_reg,strlen(test_reg),ip_tejo,dns_port);
-  /*
+
+  // validar argumentos de chamada
   if(argc < 9){
     wrong_use();
+  }else{
+    if(strcmp(argv[1],"-n")==0){
+      strcpy(name,argv[2]);
+    }else{
+      wrong_use();
+    }
+    if(strcmp(argv[3],"-j")==0){
+      strcpy(ip,argv[4]);
+    }else{
+      wrong_use();
+    }
+    if(strcmp(argv[5],"-u")==0){
+      strcpy(upt,argv[6]);
+    }else{
+      wrong_use();
+    }
+    if(strcmp(argv[7],"-t")==0){
+      strcpy(tpt,argv[8]);
+    }else{
+      wrong_use();
+    }
+    // falta adicionar os opcionais
   }
-  */
-
-
- /*
-      Adicionar parte para validar argv
-  */
 
   // 1 -  Registar servidor de mensagens no sevidor de identidades
   // 2 - interrogar o servido de mensagens sobre a identidade dos outros servidores de mensagens
   //  e estabelecer TCP entre cada um destes
   // Ir buscar todas as mensagens
   //
+  //
+
+  // mudar isto para ir buscar o ip
+  int fd_id_server = udp_connect(ip_tejo,dns_port);
   while(1){
-    printf("Enter a message: \n");
+    printf("Enter a command:  ");
     if(fgets(buffer, BUFFERSIZE , stdin) != NULL)
     {
       size_t ln = strlen(buffer)-1;
@@ -58,10 +76,12 @@ int main(int argc, char *argv[]){
           printf("Show mensagens\n");
 
       }else if(strcmp("join",command)==0){
+        sprintf(test_reg,"REG %s;%s;%s;%s",name,ip,upt,tpt);
+        udp_write_to(fd_id_server,test_reg,strlen(test_reg),ip_tejo,dns_port);
         printf("Go Registar\n");
 
       }else if(strcmp("exit",command)==0){
-        printf("exit");
+        exit(0);
       }else{
         printf("Unkown command\n");
       }
---
+++ project/run_msgserv.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./msgserv -n name -j 127.0.0.1 -u 17000 -t 18000
