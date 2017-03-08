4fb270eddecca140e6ca097b8211060048275dc1
Cris <cristina.rodrigues.melicio@gmail.com>
2017-03-08 19:10:56 +0000

    get_servers no task3

--- labguide/task3_udplab.c
+++ labguide/task3_udplab.c
@@ -9,12 +9,10 @@
 #include <unistd.h>
 int main(void)
 {
-int fd, n1, n2, addrlen;
+int fd, n, addrlen;
 struct sockaddr_in addr;
 struct in_addr iaddr;
 char buffer[128];
-//char buffer2[128];
-
 
 inet_aton("193.136.138.142",&iaddr);
 fd=socket(AF_INET,SOCK_DGRAM,0);//UDP socket
@@ -25,31 +23,20 @@ memset((void*)&addr,(int)'\0',sizeof(addr));
 addr.sin_family=AF_INET;
 addr.sin_addr=iaddr;
 
-addr.sin_port=htons(58000);
-n1=sendto(fd,"Hello cris!\n",12,0,(struct sockaddr*)&addr,sizeof(addr));
-n2=sendto(fd,"Hello!\n",7,0,(struct sockaddr*)&addr,sizeof(addr));
+addr.sin_port=htons(59000);
+n=sendto(fd,"GET_SERVERS\n",12,0,(struct sockaddr*)&addr,sizeof(addr));
 
-if(n1==-1)
-  exit(1);//error
-
-if(n2==-1)
+if(n==-1)
   exit(1);//error
 
 addrlen=sizeof(addr);
 
-n1=recvfrom(fd,buffer,128,0,(struct sockaddr*)&addr,&addrlen);
+n=recvfrom(fd,buffer,128,0,(struct sockaddr*)&addr,&addrlen);
 write(1,"echo: ",6);
-write(1,buffer,n1);
+write(1,buffer,n);
 
-
-if(n1==-1)
+if(n==-1)
   exit(1);
-n2=recvfrom(fd,buffer,128,0,(struct sockaddr*)&addr,&addrlen);
-if(n2==-1)
-  exit(1);
-
-write(1,"echo: ",6);
-write(1,buffer,n2);
 
 close(fd);
 exit(0);
