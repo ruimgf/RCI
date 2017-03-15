2182d16784bc1dd8e7349e042d2b2c182d855038
Rui Figueiredo <ruimgf95@gmail.com>
2017-03-15 20:01:10 +0000

    select not tested

--- project/msgserv.c
+++ project/msgserv.c
@@ -5,24 +5,133 @@
 
 
 #define BUFFERSIZE 100
+#define MESSAGESIZE 140
+
+
 
 void wrongUse(){
   printf("Wrong Program Usage : msgserv –n name –j ip -u upt –t tpt [-i siip] [-p sipt] [–m m] [–r r] \n");
   //exit(-1);
 }
 
+typedef struct messageBox_{
+    int actual_pos; // actual first free position
+    char ** messages; // matrix of messages
+    int length; // max length off box
+}messageBox;
+
+messageBox * m;
+
+messageBox * createMessageBox(int length){
+      messageBox * m;
+      int i;
+      m = malloc(sizeof(messageBox));
+      m->actual_pos = 0;
+      m->length = length;
+      m->messages = malloc(length * sizeof(char *));
+      for(i=0;i<MESSAGESIZE;i++){
+        m->messages[i] = malloc(MESSAGESIZE * sizeof(char));
+      }
+      return m;
+}
+
+void saveMessage(messageBox * m ,char * message){
+     sprintf(m->messages[m->actual_pos],"%s",message);
+     m->actual_pos = m->actual_pos + 1;
+}
+
+void printAllMessages(messageBox * m){
+    int i;
+    // cuidado se ja deu a volta ou nao
+    for (i = 0; i < m->actual_pos; i++) {
+      printf("%s\n",m->messages[i]);
+    }
+}
+
+void readRmb(int fdIdServer){
+  char buffer[200];
+  char command[50];
+  int n = udpRead(fdIdServer,buffer,BUFFERSIZE);
+  printf("%s\n",buffer);
+  sscanf(buffer,"%s",command);
+  if(strcmp(command,"PUBLISH")==0){
+    char message[140];
+    strncpy(message, buffer+8, 140);
+    saveMessage(m,message);
+  }
+}
+
+void destructMessageBox(messageBox * m){
+    int i;
+    for ( i = 0; i < m->length; i++) {
+      free(m->messages[i]);
+    }
+    free(m->messages);
+    free(m);
+}
+
+int udpServer(int port){
+  int fd = socket(AF_INET,SOCK_DGRAM,0);
+  struct sockaddr_in serveraddr;
+
+  memset((void*)&serveraddr,(int)'\0',sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serveraddr.sin_port = htons((u_short)port);
+
+  bind(fd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+  return fd;
+}
+
+// isto ainda é para mudar
+char name[100];
+char ip[100];
+char upt[100];
+char tpt[100];
+char ip_tejo[]  = "193.136.138.142";
+int dns_port = 59000;
+char test_reg[100];
+
+void keyboardRead(int fdIdServer){
+  char buffer[BUFFERSIZE];
+  char command[BUFFERSIZE];
+
+  if(fgets(buffer, BUFFERSIZE , stdin) != NULL)
+  {
+    size_t ln = strlen(buffer)-1;
+    if (buffer[ln] == '\n'){
+        buffer[ln] = '\0';
+    }
+    sscanf(buffer,"%s",command);
+
+    if(strcmp("show_servers",command)==0){
+      printf("Show Servers\n");
+    }else if(strcmp("show_messages",command)==0){
+      printAllMessages(m);
+    }else if(strcmp("join",command)==0){
+      sprintf(test_reg,"REG %s;%s;%s;%s",name,ip,upt,tpt);
+      udpWriteTo(fdIdServer,test_reg,strlen(test_reg),ip_tejo,dns_port);
+      // pode implementar-se um read para ver se foi registado com sucesso
+      printf("Go Registar\n");
+    }else if(strcmp("exit",command)==0){
+      destructMessageBox(m);
+      close(fdIdServer);
+      exit(0);
+    }else if(strcmp("ler",command)==0){
+
+    }else{
+      printf("Unkown command\n");
+    }
+  }
+}
 int main(int argc, char *argv[])
 {
   char buffer[BUFFERSIZE];
   char command[BUFFERSIZE];
-  char name[100];
-  char ip[100];
-  char upt[100];
-  char tpt[100];
-  char ip_tejo[]  = "193.136.138.142";
-  int dns_port = 59000;
-  char test_reg[100];
 
+  fd_set rfds;
+
+  m = createMessageBox(200);
 
   // trocar a ordem disto, pode aparecer por outras ordens
   if(argc < 9){
@@ -57,39 +166,32 @@ int main(int argc, char *argv[])
   // Ir buscar todas as mensagens
 
   // mudar isto para ir buscar o ip
-  int fd_id_server = udpConnect();
+  int fdIdServer = udpServer(5000);
+
 
   while(1){
-    printf("Enter a command:  ");
-    if(fgets(buffer, BUFFERSIZE , stdin) != NULL)
-    {
-      size_t ln = strlen(buffer)-1;
-      if (buffer[ln] == '\n'){
-          buffer[ln] = '\0';
-      }
-      sscanf(buffer,"%s",command);
+    FD_ZERO(&rfds);
+    FD_SET(fdIdServer,&rfds);
+    FD_SET(1,&rfds);
+    int maxfd=fdIdServer;
+    int counter;
 
-      if(strcmp("show_servers",command)==0){
+    counter=select(fdIdServer+1,&rfds,(fd_set*)NULL,(fd_set*)NULL,(struct timeval *)NULL);
 
-        printf("Show Servers\n");
+    if(counter<=0)exit(1);//errror
 
-
-      }else if(strcmp("show_messages",command)==0){
-          printf("Show mensagens\n");
-
-      }else if(strcmp("join",command)==0){
-        sprintf(test_reg,"REG %s;%s;%s;%s",name,ip,upt,tpt);
-        udpWriteTo(fd_id_server,test_reg,strlen(test_reg),ip_tejo,dns_port);
-        printf("Go Registar\n");
-
-      }else if(strcmp("exit",command)==0){
-        exit(0);
-      }else if(strcmp("ler",command)==0){
-
-      }else{
-        printf("Unkown command\n");
-      }
+    if(FD_ISSET(fdIdServer,&rfds)){
+      readRmb(fdIdServer);
+    }else if(FD_ISSET(1,&rfds)){
+      keyboardRead(fdIdServer);
+      printf("Enter a command:  ");
     }
 
+
+
+
+
+
   }
+
 }
---
+++ project/test/writemsg.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nc -u 127.0.0.1 5000 < messages 
